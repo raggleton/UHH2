3495a84b7c7e0f6aaa13d3a5a8140f692e2f26fb
Robin Aggleton <robin.aggleton@cern.ch>
2018-02-12 14:34:01 +0100

    Fix segfault as non const ?

--- core/include/GenJetWithParts.h
+++ core/include/GenJetWithParts.h
@@ -14,8 +14,8 @@ class GenJetWithParts : public FlavorParticle {
   ~GenJetWithParts(){
   };
 
-  std::vector<unsigned int> genparticles_indices() const{return m_genparticles_indices;}
-  void add_genparticles_index(int ind){m_genparticles_indices.push_back(ind);}
+  const std::vector<unsigned int> & genparticles_indices() const{return m_genparticles_indices;}
+  void add_genparticles_index(const int ind){m_genparticles_indices.push_back(ind);}
 
  private:
 std::vector<unsigned int> m_genparticles_indices;
