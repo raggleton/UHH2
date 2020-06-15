942be45a6c99f92799f13551fdff2f40827b18ec
Robin Aggleton <robin.aggleton@cern.ch>
2020-06-15 12:11:13 +0200

    Return false if event doesn't have trigger, instead of throwing

--- core/src/Event.cxx
+++ core/src/Event.cxx
@@ -103,7 +103,8 @@ string format_list(const vector<string> & l){
 
 bool Event::passes_trigger(TriggerIndex & ti) const{
     if(!lookup_trigger_index(ti)){
-        throw runtime_error("Event does not have trigger '" + ti.triggername + "'. Available triggers:\n" + format_list(triggerNames_currentrun));
+        return false;
+        // throw runtime_error("Event does not have trigger '" + ti.triggername + "'. Available triggers:\n" + format_list(triggerNames_currentrun));
     }
     if(!triggerResults){
         throw runtime_error("Event::passes_trigger: trigger Results have not beed read in");
